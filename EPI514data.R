8549876cb160766d74fcc1790fcae50df6b9200a
caitlindrover <caitlindrover@gmail.com>
2022-04-28 15:35:29 -0700

    data cleaning progress

--- EPI514data.R
+++ EPI514data.R
@@ -22,24 +22,57 @@ library(tidyverse)
 library(haven)
 setwd("/Users/NicoleBriggs 1/Documents/MPH Coursework/SpringQ2022/epi514")
 dataDir <- "/Users/NicoleBriggs 1/Documents/MPH Coursework/SpringQ2022/epi514"
-
 dataraw <- read_xpt("LLCP2019.XPT ")
-write.csv(dataraw, paste0(dataDir, "LLCP2019.XPT"), row.names = FALSE)
-dataraw <- read.csv(paste0(dataDir, "LLCP2019.XPT"))
+write.csv(dataraw, paste0(dataDir, "LLCP2019.csv"), row.names = FALSE)
+dataCSV <- read.csv(paste0(dataDir, "LLCP2019.csv"))
 
-data <- dataraw[, c("_PSU", "_LLCPWT", "_AGE_G" )]
-names(data) #view variable names 
-summary(data) #check for weird values 
+# trimming the dataset 
+data <- dataCSV[, c("X_STATE", "X_PSU", "X_STSTR", "X_LLCPWT",
+                    "ACEDIVRC", "SEXVAR", 
+                    "X_AGEG5YR", "X_AGE80", "X_AGE_G",
+                    "INCOME2", "X_INCOMG", "EDUCA", "X_EDUCAG", 
+                    "X_HISPANC", "X_RACE", "X_RACEGR3",
+                    "FLUSHOT7")]
+# key variables for analysis: ACEDIVRC, FLUSHOT7, X_AGE_G, X_RACE or X_RACEGR3?, 
+# SEXVAR, X_INCOMG, X_EDUCAG
+names(data)
+summary(data)
 
 ## data cleaning/recoding  
-#sex variable 
-data$X_SEX[data$X_SEX==9] <- NA
+# exposure divorce
+data$divorce = data$ACEDIVRC
+data$divorce[data$divorce==7 | data$divorce==9] <- NA
+data$divorce[data$divorce==1] <- 1
+data$divorce[data$divorce==2] <- 0
+# outcome flu shot 
+data$vaccinated = data$FLUSHOT7
+data$vaccinated[data$FLUSHOT7==7 | data$FLUSHOT7==9] <- NA
+data$vaccinated[data$FLUSHOT7==1] <- 1
+data$vaccinated[data$FLUSHOT7==2] <- 0
+# sex 
+data$sex = data$X_SEX
+data$sex[data$sex==9] <- NA
 data$sexFac <- factor(data$X_SEX, 
                       levels = 1:2, 
                       labels = c("Male", "Female"))
 data$male[data$X_SEX==2] <- 0
 data$male[data$X_SEX==1] <- 1
-
-
+# age 
+data$age5yrFac = data$X_AGE_G
+data$age5yrFac[data$age5yrFac==14] <- NA
+data$age5yrFac <- factor(data$age5yrFac, levels = 1:13,
+                         labels = c("18-24", "25-29", "30-34",
+                                    "35-39", "40-44", "45-49",
+                                    "50-54", "55-59", "60-64",
+                                    "65-69", "70-74", "75-79", "80+"))
+# race/ethnicity 
+data$raceFac = data$X_RACE
+data$raceFac[data$raceFac==9] <- NA 
+data$raceFac <- factor(data$raceFac, levels = 1:8,
+                       labels = c("White non-Hispanic", "Black non-Hispanic",
+                                  "AI/AN non-Hispanic", "Asian non-Hispanic",
+                                  "Native Hawaiian non-Hispanic",
+                                  "Other race non-Hispanic", "Multiracial non-Hispanic",
+                                  "Hispanic"))
 
 
